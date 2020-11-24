87f0b9222dab31ec571da2624f12b2f192320713
Moulay Ahmed <ahmed.elidrissi.attach@gmail.com>
2020-11-24 07:31:36 +0100

    Set It Up

---
+++ src/test/java/org/aei/bank/account/BankAccountTest.java
@@ -0,0 +1,15 @@
+package org.aei.bank.account;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountTest {
+
+    @Test
+    public void testItSUp() {
+        assertEquals(1, 1);
+    }
+
+}
